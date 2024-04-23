d2a8e35066f3c7fe92fbaf8e76130545956c0c3f
Jake Bryan <60036695+jakebry@users.noreply.github.com>
2024-04-22 23:03:39 -0700

    Updated Only Passes Fuction

--- starttask.py
+++ starttask.py
@@ -281,7 +281,7 @@ def scrape_ebay():
 
         if not any_steals:  # Add this block
             print("Only Passes")
-            insert_item_details(gc, "Only Passes", "0", "0", "0", "0", "0")
+            insert_item_details(gc, "Only Passes", "0", "0", "0", "0")
 
         driver.quit()
     except Exception as e:
